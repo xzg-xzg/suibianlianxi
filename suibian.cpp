192cfe9a0ecc6a5ce7f08c39c8a0226d7fae0c14
xzg-xzg <995832402@qq.com>
2020-03-06 16:20:04 +0800

    第一次

---
+++ suibian.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<windows.h>
+using namespace std;
+int main(void) {
+	int s=1;
+
+	for(int i = 1; i <= 100; i++) {
+		s+= i;
+	}
+	system("pause");
+	return 0;
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
